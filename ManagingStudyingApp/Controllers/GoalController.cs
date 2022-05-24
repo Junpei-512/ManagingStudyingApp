bf1919b81ff8db7f5c859b0c6cff35db4c02e491
Junpei-512 <junpei.910512.sunsys.cst@gmail.com>
2022-05-24 16:11:37 +0900

    2022/05/24 学習管理アプリ初回コミット

---
+++ ManagingStudyingApp/Controllers/GoalController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ManagingStudyingApp.Controllers
+{
+    public class GoalController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            ViewData["GoalIndexMessage"] = "Please set or edit your goals in here.";
+            ViewData["SetGoalsMessage"] = "Please set your goals in here.";
+            ViewData["EditGoalsMessage"] = "Please edit your goals in here.";
+            return View();
+        }
+
+        public IActionResult SetGoals()
+        {
+            ViewData["SetGoalsMessage"] = "Please set your goals in here.";
+            return View();
+        }
+    }
+}
